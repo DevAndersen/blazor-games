b147ce24e47c31eb10253de904ee45f0879e2404
DevAndersen <martinba.private@gmail.com>
2022-10-30 18:02:35 +0100

    BasePage, shared page title logic

---
+++ src/DevAndersen.BlazorGames.Site/ViewExtensions/BasePage.cs
@@ -0,0 +1,18 @@
+﻿using DevAndersen.BlazorGames.Site.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace DevAndersen.BlazorGames.Site.ViewExtensions;
+
+public abstract class BasePage : ComponentBase
+{
+    public virtual string PageTitle { get; } = "Blazor Games";
+
+    [CascadingParameter]
+    public MainLayout MainLayout { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        MainLayout?.UpdatePageTitle(PageTitle);
+    }
+}
