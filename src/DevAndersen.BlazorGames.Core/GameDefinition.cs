915f5d5122f3ed300f4b4fba62e93d5f1347cfb0
DevAndersen <martinba.private@gmail.com>
2022-10-30 18:30:30 +0100

    Consecutive chat messages from the same user are now grouped into one chat bubble

--- src/DevAndersen.BlazorGames.Core/GameDefinition.cs
+++ src/DevAndersen.BlazorGames.Core/GameDefinition.cs
@@ -23,7 +23,7 @@ public class GameDefinition
 
     public static readonly IEnumerable<GameDefinition> GameDefinitions = new GameDefinition[]
     {
-        new GameDefinition(GameIdentity.RockPaperScissors, "/games/RockPaperScissors", "Rock paper scissors", 1, new string(new char[]
+        new GameDefinition(GameIdentity.RockPaperScissors, "/games/RockPaperScissors", "Rock paper scissors", 2, new string(new char[]
         {
             (char)55358, (char)57000, // Rock emoji
             (char)55357, (char)56540, // Scroll emoji
