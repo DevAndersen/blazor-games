0412277ee89283de64a4260132ccaf781adc958b
DevAndersen <martinba.private@gmail.com>
2022-10-30 13:53:14 +0100

    Username support

--- src/DevAndersen.BlazorGames.Core/Players/PlayerIdentity.cs
+++ src/DevAndersen.BlazorGames.Core/Players/PlayerIdentity.cs
@@ -1,10 +1,20 @@
-﻿namespace DevAndersen.BlazorGames.Core.Players;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-public class PlayerIdentity
+namespace DevAndersen.BlazorGames.Core.Players;
+
+public class PlayerIdentity : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
     public Guid Id { get; set; } = default!;
 
-    public string? Username { get; set; }
+    private string? username;
+    public string? Username
+    {
+        get => username;
+        set => SetOnPropertyChanged(ref username, value);
+    }
 
     /// <summary>
     /// Necessary for deserialization to work.
@@ -16,5 +26,16 @@ public class PlayerIdentity
     public PlayerIdentity(Guid id)
     {
         Id = id;
+    }
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public void SetOnPropertyChanged<T>(ref T property, T value, [CallerMemberName] string propertyName = "")
+    {
+        property = value;
+        OnPropertyChanged(propertyName);
     }
 }
